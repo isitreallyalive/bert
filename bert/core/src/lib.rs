5bd0bd6f3502abf0e1cb6900bb43001010fd1a4b
newt <hi@newty.dev>
2025-06-09 01:58:05 +0100

    feat: primitive module trait

--- bert/core/src/lib.rs
+++ bert/core/src/lib.rs
@@ -1,1 +1,25 @@
+pub mod prelude {
+    pub use crate::{Module, create_module};
+}
 
+/// Module trait that dynamically loaded modules must implement.
+pub trait Module {
+    /// Instantiate the module.
+    fn new() -> Self
+    where
+        Self: Sized;
+
+    /// The name of the module.
+    fn name(&self) -> &str;
+}
+
+#[macro_export]
+macro_rules! create_module {
+    ($module_type:ty) => {
+        #[unsafe(no_mangle)]
+        pub unsafe extern "C" fn create_module() -> *mut dyn $crate::Module {
+            let module = <$module_type>::new();
+            Box::into_raw(Box::new(module)) as *mut dyn $crate::Module
+        }
+    };
+}
--- modules/base/src/lib.rs
+++ modules/base/src/lib.rs
@@ -1,1 +1,15 @@
+use bert::prelude::*;
 
+pub struct Base;
+
+impl Module for Base {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn name(&self) -> &str {
+        "base"
+    }
+}
+
+create_module!(Base);
